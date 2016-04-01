e0a82453658da2bfc49409d6ecf8b30951c3e91c
jacethelefty <jacemontoya@live.com>
2016-04-01 09:00:15 -0700

    initial portfolio push

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html,body').animate({
+               scrollTop: target.offset().top - 50
+          }, 1000);
+          return false;
+      }
+  }
+});
+
+/* =============== hide container on scroll =============== */
+
+$(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    var height = ($(this).height()) - 500;
+    $('#container1').css({
+      'opacity': ((height - scrollTop) / height)
+    });
+});
+
+/* =============== width on scroll =============== */
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > '980') {
+      $("#skl1, #skl2, #skl3, #skl4, #skl5, #skl6, #skl7, #skl8").show(2700, 'swing');
+    }else{
+      $("#skl1, #skl2, #skl3, #skl4, #skl5, #skl6, #skl7, #skl8").hide(1000);
+    }
+});
