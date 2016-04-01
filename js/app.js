753dbbce4691fd0569f86934a069574b72d01894
jacethelefty <jacemontoya@live.com>
2016-04-01 09:36:50 -0700

    working on fixing mobile issue #6

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $('a[href*=#]:not([href=#])').click(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
          if (target.length) {
            $('html,body').animate({
-               scrollTop: target.offset().top - 4
+               scrollTop: target.offset().top + 5
           }, 1000);
           return false;
       }
