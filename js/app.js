c791b758be0459c8dea0d2497ebb7d39a2f985e4
jacethelefty <jacemontoya@live.com>
2016-04-04 11:14:35 -0700

    implement hamburger navigation step #2

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $('a[href*=#]:not([href=#])').click(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
          if (target.length) {
            $('html,body').animate({
-               scrollTop: target.offset().top + 10
+               scrollTop: target.offset().top + 0
           }, 1000);
           return false;
       }
